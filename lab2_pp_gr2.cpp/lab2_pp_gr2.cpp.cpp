525ab1c8376c8ddcdddb9785eb52b73084b6942c
hanna2306 <hanna.dota23@gmail.com>
2025-10-15 14:41:44 +0200

    zadanie 8

--- lab2_pp_gr2.cpp/lab2_pp_gr2.cpp.cpp
+++ lab2_pp_gr2.cpp/lab2_pp_gr2.cpp.cpp
@@ -2,12 +2,17 @@
 #include <iostream>
 using namespace std;
 int main() {
-	int x[9];
+
+	for (int i = 0; i <= 100; i = i + 2) {
+		cout << i << endl;
+	}
+
+
+
+	/*int x[9];
 	for (int i = 0; i < 10; i++) {
 		cout << "Podaj liczbê rzeczywist¹: " << endl;
 		cin >> x[i];
 			if (x[i] > 0) {
-			cout << x[i] << endl; }
-
-	}
+			cout << x[i] << endl; }}*/
 }
