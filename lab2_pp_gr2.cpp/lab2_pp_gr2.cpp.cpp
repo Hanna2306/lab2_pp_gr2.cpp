3e9490339c9e408ff17426246f52397eee8a9ba7
hanna2306 <hanna.dota23@gmail.com>
2025-10-15 14:46:23 +0200

    zadanie 9

--- lab2_pp_gr2.cpp/lab2_pp_gr2.cpp.cpp
+++ lab2_pp_gr2.cpp/lab2_pp_gr2.cpp.cpp
@@ -2,14 +2,25 @@
 #include <iostream>
 using namespace std;
 int main() {
+	int n;
+	cout << "Podaj liczbê n: " << endl;
+	cin >> n;
 
-	for (int i = 0; i <= 100; i = i + 2) {
+	for (int i = 0; i <= 100; i++) {
+		if (i%n == 0)
+			cout << i << endl;
+	}
+	
+	
+	
+	
+	
+	
+	
+	/*for (int i = 0; i <= 100; i = i + 2) {
 		cout << i << endl;
 	}
-
-
-
-	/*int x[9];
+	int x[9];
 	for (int i = 0; i < 10; i++) {
 		cout << "Podaj liczbê rzeczywist¹: " << endl;
 		cin >> x[i];
