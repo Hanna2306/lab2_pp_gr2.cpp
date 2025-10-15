c9d0776d5697b1a07e31fcac9cf8c94c834f9c47
hanna2306 <hanna.dota23@gmail.com>
2025-10-15 14:00:43 +0200

    zadanie 1 podpunkt 2

--- lab2_pp_gr2.cpp/lab2_pp_gr2.cpp.cpp
+++ lab2_pp_gr2.cpp/lab2_pp_gr2.cpp.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 using namespace std;
 int main() { //kod 02_12
-for (int i = 0; i <= 10; i++) {
- cout << i << endl;}
+	for (int i = 5; i <= 7; i++) {
+		cout << i << endl;
+	} /*wyœwitlanie zbioru [5, 7] */
+
+	cout << endl;
+for (int i = 5; i < 7; i++) {
+	cout << i << endl; 
+} /*wyœwietlanie zbioru [5, 7)*/
 	 
 return 0;	
 }
