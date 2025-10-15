79ea99e15938a8ec16d229e5f06734373836ded7
hanna2306 <hanna.dota23@gmail.com>
2025-10-15 14:38:45 +0200

    Zadanie 6

--- lab2_pp_gr2.cpp/lab2_pp_gr2.cpp.cpp
+++ lab2_pp_gr2.cpp/lab2_pp_gr2.cpp.cpp
@@ -2,37 +2,12 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int fahr; //zmienne typu int 
-    int start, limit, krok; //zmienne typu int 
+	int x[9];
+	for (int i = 0; i < 10; i++) {
+		cout << "Podaj liczbê rzeczywist¹: " << endl;
+		cin >> x[i];
+			if (x[i] > 0) {
+			cout << x[i] << endl;}
 
-    start = 0; //przypisz 0 do start 
-   // limit = 200; przypisz 200 do limit 
-   // krok = 20; przypisz 20 do krok 
-    cout << "Podaj wartosc limit: " << endl;
-    cin >> limit >> endl;
-    cout << "Podaj wartosc krok: " << endl;
-    cin >> krok >> endl;
-
-
-    fahr = start; //przypisz wartosc start do zmiennej fahr, czyli fahr = 0
-
-    
-    
-        for (fahr = 0.0; fahr <= limit; fahr = fahr + krok)
-          {
-          
-            float celsius = (5/9)*(fahr - 32.0); // przy zmianie z 5.0/9.0 przestajemy otzrymywaæ logiczne wyniki
-            printf("%3.0f %6.1f \n", fahr, celsius);
-          }
-    
-   
-   
-
-    /*while (fahr <= limit) { //wykonuj petle dopoki wartosc fahr jest <= od zmiennej limit 
-        celsius = 5 * (fahr - 32) / 9; //oblicz stopnie C i przypisz wynik do celsius 
-        cout << fahr << "\t" << celsius << endl; //wypisz zmienne na ekran
-        fahr = fahr + krok; //zwieksz wartosc zmiennej fahr o wartosc zmiennej krok*/
-    }
-
-	 
-	
+	}
+}
